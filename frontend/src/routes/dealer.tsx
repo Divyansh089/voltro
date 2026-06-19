371c5b24f3cab302d5fba80250fc11067cf3ca1a
Divyansh Patel <pateldivyansh131@gmail.com>
2026-06-19 11:01:24 +0530

    style update

--- frontend/src/routes/dealer.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AdminShell } from "@/components/AdminShell";
-
-export const Route = createFileRoute("/dealer")({
-  component: () => <AdminShell portal="Dealer" />,
-});
