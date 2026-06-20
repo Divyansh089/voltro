a950792eb941ab33ecf239e32be845f0ae27d818
Divyansh Patel <pateldivyansh131@gmail.com>
2026-06-20 10:37:51 +0530

    improve perf

---
+++ backend/src/common/helpers/index.ts
@@ -0,0 +1,10 @@
+export { slugify, generateUniqueSlug } from './slug.helper';
+export { hashPassword, comparePassword } from './hash.helper';
+export {
+  calculatePagination,
+  calculateOffset,
+  DEFAULT_PAGE,
+  DEFAULT_LIMIT,
+  MAX_LIMIT,
+  type IPaginationQuery,
+} from './pagination.helper';
