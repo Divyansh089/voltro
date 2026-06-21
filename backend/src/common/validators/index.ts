ced8d97d19a6bbf8d5a859d0ee241e6138c28f10
Divyansh Patel <pateldivyansh131@gmail.com>
2026-06-21 11:45:54 +0530

    refactor

---
+++ backend/src/common/validators/index.ts
@@ -0,0 +1,13 @@
+export {
+  uuidSchema,
+  emailSchema,
+  passwordSchema,
+  phoneSchema,
+  slugSchema,
+  paginationSchema,
+  sortSchema,
+  searchSchema,
+  listQuerySchema,
+  idParamSchema,
+  idOrSlugParamSchema,
+} from './common.validator';
