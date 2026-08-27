ca7b5df2481064fce98b14cfd39a0fafc6dbcc0d
Divyansh Patel <pateldivyansh131@gmail.com>
2026-08-27 11:11:33 +0530

    update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     disableStaticImages: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
